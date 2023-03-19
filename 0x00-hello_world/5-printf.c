eb6cb5ae3aee6d2b3fed819b035f1282cf46b75d
Kingsley <kenhancer@aol.com>
2023-03-19 02:01:01 -0700

    5th test

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ *main - where main program runs
+ *Return : returns 0 if program runs succesfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
