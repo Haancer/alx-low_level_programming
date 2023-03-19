d0634ec5c8f08fdf112eae9d5ffc696bf0924fd9
Kingsley <kenhancer@aol.com>
2023-03-19 08:11:28 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123 ; i++)
+	{
+		if (i == 101 || i == 113)
+		{
+			continue;
+		}
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
