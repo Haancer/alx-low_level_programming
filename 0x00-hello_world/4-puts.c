de9fc4c7b1e6e405ea7cc8f487ac9de5096d8e6f
Kingsley <kenhancer@aol.com>
2023-03-19 01:42:48 -0700

    name c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+
+/**
+ *main - where the program runs
+ *Return: Returns 0 if there are no errors
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
