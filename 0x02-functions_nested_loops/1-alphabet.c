f8ebcbc6dd41fb8856f73adfd8f571eda98cb6fe
Kingsley <kenhancer@aol.com>
2023-03-21 23:13:02 -0700

    printing lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets in lowercase
+ *
+ * Return: Returns 0 if program runs successufuly
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
