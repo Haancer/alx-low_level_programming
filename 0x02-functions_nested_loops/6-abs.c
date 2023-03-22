7773f01eebd90be4f6c01f8c32b3d5de269259a5
Kingsley <kenhancer@aol.com>
2023-03-22 07:35:25 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_abs - print absolute value
+*
+*@a: integer value
+*
+*Return: returns 0 if prog runs smoothly
+*/
+
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+}
