a568f3ef61387996891fb3c05b84070db78a1cb4
Kingsley <kenhancer@aol.com>
2023-03-22 10:20:51 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_last_digit - prints last number
+*
+*Return: returns 0 if prog runs smoothly
+*/
+
+int print_last_digit(int a)
+{
+	int i;
+
+	i = a % 10;
+	if (i < 0)
+	{
+		_putchar(-i + 48);
+		return (-i);
+	}
+	else
+	{
+		_putchar(i + 48);
+		return (i);
+	}
+}
